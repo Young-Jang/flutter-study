0ee828775bb488d3d551ae537a8ebddc17f94e84
b116104@naver.com <b116104@naver.com>
2023-01-10 21:08:42 +0900

    feat: first practice

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,17 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: Container(child:Text('앱임'),alignment: Alignment.topLeft,)),
         body: Container(child: Text('안녕'),alignment:Alignment.topLeft,),
-        bottomNavigationBar: Container(child:Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Icon(Icons.phone),
-            Icon(Icons.message),
-            Icon(Icons.contact_page),
-          ],
+        bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+            height: 70,
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page),
+              ],
+            ),
         ),color: Colors.black12,
         ),
       )
