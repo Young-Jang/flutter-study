49508f305a96bfcd1eb32b3ad936f209091aad7b
b116104@naver.com <b116104@naver.com>
2023-01-11 23:24:37 +0900

    feat: add widget function

--- lib/main.dart
+++ lib/main.dart
@@ -12,41 +12,23 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(actions : [Icon(Icons.find_in_page_outlined),Icon(Icons.menu),Icon(Icons.alarm_rounded)],
-          title: Row(children: <Widget>[
-            Text("도림동3가"),
-            Icon(Icons.star),
-          ],),),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(20),
-          child: Row(
-            children: [
-              Image.asset('London.jpg',width: 150,),
-              Container(
-                padding: EdgeInsets.fromLTRB(10, 5, 0, 0),
-                width: 300,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('런던의 야경입니다.'),
-                    Text('도림동'),
-                    Text('7000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text('4')
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        )
+        appBar: AppBar(),
+        body: ShopItem(),
       )
     );
   }
+
 }
+
+class ShopItem extends StatelessWidget {
+  const ShopItem({Key? key}) : super(key: key);
+
+  @override
+  build(context) {
+    return SizedBox(
+      child: Text('안녕'),
+    );
+  }
+}
+
 
