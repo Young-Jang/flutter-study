4c5e6dbd25b56d66584b07d138926dfd192f8f92
b116104@naver.com <b116104@naver.com>
2023-01-17 23:15:09 +0900

    feat: add play bar & info icon

--- lib/widget/carousel_slider.dart
+++ lib/widget/carousel_slider.dart
@@ -2,13 +2,15 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import '../model/model_movie.dart';
 
-class CarouselImage extends StatefulWidget{
+class CarouselImage extends StatefulWidget {
   final List<Movie> movies;
+
   CarouselImage({required this.movies});
+
   _CarouselImageState createState() => _CarouselImageState();
 }
 
-class _CarouselImageState extends State<CarouselImage>{
+class _CarouselImageState extends State<CarouselImage> {
   late List<Movie> movies;
   late List<Widget> images;
   late List<String> keywords;
@@ -17,17 +19,17 @@ class _CarouselImageState extends State<CarouselImage>{
   late String _currentKeyword;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     movies = widget.movies;
-    images = movies.map((m) => Image.asset( m.poster)).toList();
+    images = movies.map((m) => Image.asset(m.poster)).toList();
     keywords = movies.map((m) => m.keyword).toList();
     likes = movies.map((e) => e.like).toList();
     _currentKeyword = keywords[0];
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: <Widget>[
@@ -37,7 +39,7 @@ class _CarouselImageState extends State<CarouselImage>{
           CarouselSlider(
             items: images,
             options: CarouselOptions(
-              onPageChanged: (index,reason){
+              onPageChanged: (index, reason) {
                 setState(() {
                   _currentPage = index;
                   _currentKeyword = keywords[_currentPage];
@@ -46,8 +48,66 @@ class _CarouselImageState extends State<CarouselImage>{
             ),
           ),
           Container(
-            child: Text(_currentKeyword),
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 3),
+            child: Text(
+              _currentKeyword,
+              style: TextStyle(fontSize: 11),
+            ),
           ),
+          Container(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      likes[_currentPage]
+                          ? IconButton(
+                              onPressed: () {}, icon: Icon(Icons.check))
+                          : IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+                      Text(
+                        '내가 찜한 콘텐츠',
+                        style: TextStyle(fontSize: 11),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.play_arrow,
+                          color: Colors.black,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(3),
+                        ),
+                        Text(
+                          '재생',
+                          style: TextStyle(color: Colors.black),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Column(
+                    children: <Widget>[
+                      IconButton(onPressed: () {}, icon: Icon(Icons.info)),
+                      Text(
+                        '정보',
+                        style: TextStyle(fontSize: 12),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+
         ],
       ),
     );
