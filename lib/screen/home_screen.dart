e4c58aa8726a08458230ffa5c3bc61b8251f29f4
b116104@naver.com <b116104@naver.com>
2023-01-22 17:44:11 +0900

    feat: add boxSlider, circleSlider

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_study/model/model_movie.dart';
+import 'package:flutter_study/widget/box_slider.dart';
 import 'package:flutter_study/widget/carousel_slider.dart';
+import 'package:flutter_study/widget/circle_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -53,7 +55,9 @@ class _HomeScreenState extends State<HomeScreen> {
             CarouselImage(movies: movies),
             TopBar(),
           ],
-        )
+        ),
+        CircleSlider(movies: movies,),
+        BoxSlider(movies: movies)
       ],
     );
   }
